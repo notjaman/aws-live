16915d5e6c7080a5a4f006b5c552e17142744f82
Tan Ja Man <132145823+notjaman@users.noreply.github.com>
2025-12-09 08:20:30 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cpukugao2jzz.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tanjaman-employee "
+customregion = "us-east-1"
